fd77ff96f4de61dcd1b62a07dd0b103ed7ee64e4
marvindeckmyn <marvindeckmyn@gmail.com>
2025-04-26 01:03:03 +0200

    Configure Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable
+     * 
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'title',
+        'body',
+        'slug',
+    ];
+
+    /** 
+     * Get the user that owns the post.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
