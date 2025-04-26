f1a4f6a81baf410834334677a801e560c9b4c285
marvindeckmyn <marvindeckmyn@gmail.com>
2025-04-27 00:28:16 +0200

    Add edit link to the posts index page

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -13,6 +13,7 @@ const successMessage = computed(() => page.props.flash?.success);
 </script>
 
 <template>
+
     <Head title="Blog Posts" />
 
     <AuthenticatedLayout>
@@ -20,17 +21,18 @@ const successMessage = computed(() => page.props.flash?.success);
             <div class="flex justify-between items-center">
                 <h2 class="font-semibold text-xl text-gray-800 leading-tight">Blog Posts</h2>
                 <Link :href="route('posts.create')">
-                    <PrimaryButton>Create New Post</PrimaryButton>
+                <PrimaryButton>Create New Post</PrimaryButton>
                 </Link>
             </div>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div v-if="successMessage" class="mb-4 p-4 bg-green-100 border border-green-400 text-green-700 rounded relative" role="alert">
+                <div v-if="successMessage"
+                    class="mb-4 p-4 bg-green-100 border border-green-400 text-green-700 rounded relative" role="alert">
                     <span class="block sm:inline">{{ successMessage }}</span>
                 </div>
-                
+
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
                         <h3 class="text-lg font-medium mb-4">Latests Posts</h3>
@@ -38,9 +40,18 @@ const successMessage = computed(() => page.props.flash?.success);
                             <li v-for="post in posts.data" :key="post.id" class="mb-4 pb-4 border-b last:border-b-0">
                                 <h4 class="text=lg font-semibold">{{ post.title }}</h4>
                                 <p class="text-sm text-gray-600 mb-2">
-                                    By {{ post.user ? post.user.name : 'Unknown Author' }} on {{ new Date(post.created_at).toLocaleDateString() }}
+                                    By {{ post.user ? post.user.name : 'Unknown Author' }} on {{ new
+                                        Date(post.created_at).toLocaleDateString() }}
                                 </p>
-                                <p>{{ post.body.substring(0,150) }}...</p>
+                                <p>{{ post.body.substring(0, 150) }}...</p>
+
+                                <div class="mt-2">
+                                    <Link :href="route('posts.edit', post.id)"
+                                        class="text-sm text-indigo-600 hover:text-indigo-900"
+                                        v-if="$page.props.auth.user.id === post.user_id">
+                                    Edit
+                                    </Link>
+                                </div>
                             </li>
                         </ul>
                     </div>
