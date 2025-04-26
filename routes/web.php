d23d10a47c786826955c7861c1a8ff5415073d77
marvindeckmyn <marvindeckmyn@gmail.com>
2025-04-26 16:46:28 +0200

    Add routes for the post creation

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,16 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth', 'verified')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/posts/create', function () {
+        return Inertia::render('Posts/Create');
+    })->name('posts.create');
+
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 });
 
 Route::get('/posts', function () {
